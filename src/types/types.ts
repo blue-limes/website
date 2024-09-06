6e62eb1597ad5e4a16b08a9ea9f9fab8387ccfcb
Marc Perrien <marc@reliably.com>
2024-09-06 11:52:54 +0200

    Start working on content
    Signed-off-by: Marc Perrien <marc@reliably.com>

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,14 @@ export interface Person {
   role: string;
   presentation: string;
 }
+
+export interface Software {
+  name: string;
+  role: string;
+  icon: string;
+}
+
+export interface Feature {
+  name: string;
+  description: string;
+}
