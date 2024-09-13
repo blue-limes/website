448e79b526955c39b35e2a478ff7449f602e1ef6
Marc Perrien <marc@reliably.com>
2024-09-13 14:36:25 +0200

    Favicon. Open Graph. Meta data
    Signed-off-by: Marc Perrien <marc@reliably.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bluelimes.dev",
   integrations: [
     icon({
       iconDir: "src/assets/icons",
